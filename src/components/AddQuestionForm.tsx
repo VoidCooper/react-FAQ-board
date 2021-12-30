bb2553ee393d98420753274305b6f21dc5862b6c
Santeri Sivula <santeri.sivula97@gmail.com>
2021-12-30 10:36:52 +0200

    Added form "validation" to add question form

---
+++ src/components/AddQuestionForm.tsx
@@ -0,0 +1,106 @@
+import { Guid } from "guid-typescript";
+import { useAppDispatch } from "hooks";
+import { QuestionDefObj } from "models/question";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { questionActions } from "store/question-slice";
+import Card from "./UI/Card";
+
+interface IError {
+  type: string;
+  title: string;
+  message: string;
+}
+
+let timeoutHandle: NodeJS.Timeout;
+
+const AddQuestionForm = () => {
+  const [inputState, setInputState] = useState("");
+  const [inputTouched, setInputTouched] = useState(false);
+  const [errorState, setErrorState] = useState({} as IError | null);
+  const questionInputRef = React.useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!inputTouched) {
+      inputValidator();
+      return;
+    }
+
+    const newQuestion = { ...QuestionDefObj };
+    newQuestion.id = Guid.create().toString();
+    newQuestion.text = inputState;
+
+    dispatch(questionActions.addQuestion(newQuestion));
+    setInputState("");
+    setInputTouched(false);
+    setErrorState(null);
+  };
+
+  const inputValidator = () => {
+    const curInput = questionInputRef.current?.value as string;
+    if (!curInput || curInput.length === 0) {
+      setErrorState({
+        type: "EMPTYINPUT",
+        title: "Question is empty!",
+        message: "Empty questions cannot be submitted!",
+      } as IError);
+    } else if (curInput.length < 10) {
+      setErrorState({
+        type: "SHORTINPUT",
+        title: "Question too short!",
+        message: "Question should be atleast 10 characters long!",
+      } as IError);
+    } else {
+      setErrorState(null);
+    }
+  };
+
+  useEffect(() => {
+    if (inputTouched) {
+      timeoutHandle = setTimeout(inputValidator, 500);
+    }
+    return () => {
+      clearTimeout(timeoutHandle);
+    };
+  }, [inputState]);
+
+  const userInputHandler = () => {
+    if (!inputTouched) {
+      setInputTouched(true);
+    }
+
+    const input = questionInputRef.current?.value as string;
+    setInputState(input);
+  };
+
+  const errorContent = (
+    <p>
+      {errorState?.title}
+      <br />
+      {errorState?.message}
+    </p>
+  );
+
+  return (
+    <Card title="Add a question!">
+      <form onSubmit={submitHandler}>
+        <label id="Question">Question: </label>
+        <input
+          id="Question"
+          type="text"
+          ref={questionInputRef}
+          onChange={userInputHandler}
+          value={inputState}
+        />
+        <br />
+        {errorState?.type && errorContent}
+        <input type="submit" value="Submit" disabled={errorState !== null} />
+      </form>
+    </Card>
+  );
+};
+
+export default AddQuestionForm;
--- src/pages/AddQuestion.tsx
+++ src/pages/AddQuestion.tsx
@@ -1,34 +1,7 @@
-import Card from "components/UI/Card";
-import { Guid } from "guid-typescript";
-import { useAppDispatch } from "hooks";
-import { QuestionDefObj } from "models/question";
-import React from "react";
-import { questionActions } from "store/question-slice";
+import AddQuestionForm from "components/AddQuestionForm";
 
 const AddQuestion = () => {
-  const questionInputRef = React.useRef<HTMLInputElement>(null);
-  const dispatch = useAppDispatch();
-
-  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const inputtedValue = questionInputRef.current?.value as string;
-    const newQuestion = {...QuestionDefObj};
-    newQuestion.id = Guid.create().toString();
-    newQuestion.text = inputtedValue;
-
-    dispatch(questionActions.addQuestion(newQuestion));
-  };
-
-  return (
-    <Card title="Add a question!">
-      <form onSubmit={submitHandler}>
-        <label id="Question">Question</label>
-        <input id="Question" type="text" ref={questionInputRef} />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </Card>
-  );
+  return <AddQuestionForm/>
 };
 
 export default AddQuestion;
